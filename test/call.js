2a6047d58204a0a7849de6c5afb54ed64d367344
Niklas Lindgren <nikc@iki.fi>
2014-12-13 00:48:12 +0200

    Update test for call

--- test/call.js
+++ test/call.js
@@ -1,24 +1,30 @@
 var test = require("tape");
+var sinon = require("sinon");
 var call = require("../call");
 
 test("call", function (t) {
-    t.test("export", function (t) {
-        t.plan(2);
-        t.equal(typeof call, "function", "exports function");
-        t.equal(typeof call.call, "function", "exports redundant api");
+    t.test("exports function", function (t) {
+        t.plan(1);
+        t.equal(typeof call, "function");
     });
 
-    t.test(function (t) {
+    t.test("exports redundant api", function (t) {
+        t.plan(2);
+        t.equal(typeof call.call, "function");
+        t.ok(call.call === call);
+    });
+
+    t.test("invokes method", function (t) {
         t.plan(1);
         var c = call("foo");
         c({
             "foo": function () {
-                t.pass("function was invoked");
+                t.pass("method was invoked");
             }
         });
     });
 
-    t.test(function (t) {
+    t.test("passes in arguments", function (t) {
         t.plan(2);
         var c = call("foo", "bar", "baz");
         c({
@@ -27,6 +33,34 @@ test("call", function (t) {
                 t.equal(a2, "baz");
             }
         });
+    });
+
+    t.test("calls function", function (t) {
+        t.plan(1);
+        var fn = sinon.spy();
+        call(fn)();
+        t.ok(fn.calledOnce);
+    });
+
+    t.test("function receives fixed arguments", function (t) {
+        t.plan(1);
+        var fn = sinon.spy();
+        call(fn, 1, 2)();
+        t.ok(fn.calledWith(1, 2));
+    });
+
+    t.test("function receives iterator arguments", function (t) {
+        t.plan(1);
+        var fn = sinon.spy();
+        call(fn)(3, 4);
+        t.ok(fn.calledWith(3, 4));
+    });
+
+    t.test("function receives merged arguments", function (t) {
+        t.plan(1);
+        var fn = sinon.spy();
+        call(fn, 1, 2)(3, 4);
+        t.ok(fn.calledWith(1, 2, 3, 4));
     });
 });
 
