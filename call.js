6adf2268649b37e7c43a19da2ab5590c3be5045f
Niklas Lindgren <nikc@iki.fi>
2014-12-13 00:47:18 +0200

    Alter behaviour depending on string/function
    - String invokes method on object with fixed args only
    - Function is invoked with fixed and call time args

--- call.js
+++ call.js
@@ -2,9 +2,41 @@ var toarray = require("./toarray");
 
 module.exports = call.call = call;
 
-function call(method) {
+function call(fn) {
     var args = toarray(arguments).slice(1);
 
+    if (typeof fn === "string") {
+        return callMethod(fn, args);
+    }
+
+    return callFunction(fn, args);
+}
+
+function callFunction(fn, _args) {
+    return function () {
+        var args = toarray(arguments).slice(0);
+
+        // Prepend fixed args when present
+        if (_args && _args.length > 0) {
+            args = _args.concat(args);
+        }
+
+        switch (args.length) {
+            case 4:
+                return fn(args[0], args[1], args[2], args[3]);
+            case 3:
+                return fn(args[0], args[1], args[2]);
+            case 2:
+                return fn(args[0], args[1]);
+            case 1:
+                return fn(args[0]);
+        }
+
+        return fn.apply(undefined, args);
+    };
+}
+
+function callMethod(method, args) {
     if (args.length > 0) {
         return function (o) {
             switch (args.length) {
