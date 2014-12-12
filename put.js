af6ba17cbcec17678a8d4c6ab545c8a159d4d49a
Niklas Lindgren <nikc@iki.fi>
2014-12-12 16:50:22 +0200

    Allow put to take a function as value
    Function receives the write target as input

--- put.js
+++ put.js
@@ -2,7 +2,13 @@ module.exports = put.put = put;
 
 function put(prop, val) {
     return function (o) {
-        o[prop] = val;
+        if (typeof val === "function") {
+            o[prop] = val(o);
+        } else {
+            o[prop] = val;
+        }
+
+        return o;
     };
 }
 
--- test/put.js
+++ test/put.js
@@ -1,4 +1,5 @@
 var test = require("tape");
+var sinon = require("sinon");
 var put = require("../put");
 
 test("put", function (t) {
@@ -7,11 +8,19 @@ test("put", function (t) {
         t.equal(typeof put, "function", "exports function");
         t.equal(typeof put.put, "function", "exports redundant api");
     });
-    t.test(function (t) {
+    t.test("assigns property to input", function (t) {
         t.plan(1);
         var putter = put("foo", "bar");
         var baz = {};
         putter(baz);
         t.equal(baz.foo, "bar");
+    });
+    t.test("accepts function as value", function (t) {
+        t.plan(2);
+        var stub = sinon.stub().returns(42);
+        var putter = put("meaningOfLifeTheUniverseAndEverything", stub);
+
+        t.equal(putter({}).meaningOfLifeTheUniverseAndEverything, 42);
+        t.ok(stub.calledOnce, "function was called");
     });
 });
