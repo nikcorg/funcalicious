867dcefe840f50543218ae67de3c6e97f32428de
Niklas Lindgren <nikc@iki.fi>
2015-05-28 15:29:35 +0300

    Add custom step to range
    Fixes leaked global

--- range.js
+++ range.js
@@ -1,9 +1,13 @@
+"use strict";
+
 var debug = require("debug")("funcalicious:range");
 var sign = require("./sign");
 
 module.exports = range.range = range;
 
-function range(from, to) {
+function range(from, to, step) {
+    var steps;
+
     if (undefined == to) {
         to = from;
         from = 0;
@@ -11,11 +15,26 @@ function range(from, to) {
 
     from = Math.round(from);
     to = Math.round(to);
-    step = sign(to - from);
 
-    debug("from %d to %d (%d) step %d", from, to, Math.abs(to - from), step);
+    if (undefined == step) {
+        step = sign(to - from);
+    }
 
-    return Array.apply(Array, Array(Math.abs(to - from))).
+    // Make sure range direction matches step sign
+    if (sign(to - from) !== sign(step)) {
+        throw new Error("Step does not match range direction");
+    }
+
+    steps = Math.abs(to - from) / Math.abs(step);
+
+    // Odd number of steps is not supported
+    if (Math.round(steps) !== steps) {
+        throw new Error("Invalid step increase for range");
+    }
+
+    debug("from %d to %d (%d) step %d = %d steps", from, to, Math.abs(to - from), step, steps);
+
+    return Array.apply(Array, Array(steps)).
         map(function (_, i) {
             return from + i * step;
         });
