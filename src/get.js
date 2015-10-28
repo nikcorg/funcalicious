6267347ccbcae7d045084232f7cc0b1b42a58677
Niklas Lindgren <nikc@iki.fi>
2015-10-28 10:52:19 +0200

    Update get

---
+++ src/get.js
@@ -0,0 +1,1 @@
+export const get = prop => ob => ob[prop];
--- test/get.js
+++ test/get.js
@@ -1,27 +1,20 @@
-var test = require("tape");
-var get = require("../get");
+import test from "tape";
+import { get } from "../src/get";
 
-test("get", function (t) {
-    t.test("exports function", function (t) {
+test("get", t => {
+    t.test("exports function", t => {
         t.plan(1);
         t.equal(typeof get, "function", "exports function");
     });
 
-    t.test("exports redundant api", function (t) {
-        t.plan(1);
-        t.ok(get.get === get);
-        t.end();
-    });
-
-    t.test("returns function", function (t) {
+    t.test("returns function", t => {
         t.plan(1);
         t.equal(typeof get("foo"), "function");
     });
 
-    t.test(function (t) {
+    t.test(t => {
         t.plan(1);
-        var getter = get("foo");
-
+        const getter = get("foo");
         t.equal(getter({"foo": "bar"}), "bar", "returns prop val");
     });
 });
