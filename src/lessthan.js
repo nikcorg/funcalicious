4c082369cf65a587a34d01375985307b4809ff48
Niklas Lindgren <nikc@iki.fi>
2015-10-28 10:53:52 +0200

    Update lessthan

---
+++ src/lessthan.js
@@ -0,0 +1,2 @@
+export const lessthan = what => i => i < what;
+
--- test/lessthan.js
+++ test/lessthan.js
@@ -1,29 +1,23 @@
-var test = require("tape");
-var lessthan = require("../lessthan");
+import test from "tape";
+import { lessthan } from "../src/lessthan";
 
-test("lessthan", function (t) {
-    t.test("exports function", function (t) {
+test("lessthan", t => {
+    t.test("exports function", t => {
         t.plan(1);
         t.equal(typeof lessthan, "function");
     });
 
-    t.test("exports redundant api", function (t) {
-        t.plan(2);
-        t.ok(lessthan.lessthan === lessthan);
-        t.ok(lessthan.lessThan === lessthan);
-    });
-
-    t.test("returns function", function (t) {
+    t.test("returns function", t => {
         t.plan(1);
         t.equal(typeof lessthan(0), "function");
     });
 
-    t.test("returns boolean", function (t) {
+    t.test("returns boolean", t => {
         t.plan(1);
         t.equal(typeof lessthan(2)(1), "boolean");
     });
 
-    t.test("returns expected", function (t) {
+    t.test("returns expected", t => {
         t.plan(3);
         t.ok(lessthan(2)(1));
         t.notOk(lessthan(2)(2));
