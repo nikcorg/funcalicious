3d8820f8531d6edeabf1bfcfd0eb53d4fc6e8f7e
Niklas Lindgren <nikc@iki.fi>
2015-10-28 11:44:57 +0200

    Update toarray

---
+++ src/toarray.js
@@ -0,0 +1,1 @@
+export const toarray = (arraylike, skip = 0) => Array.from(arraylike).slice(skip);
--- test/toarray.js
+++ test/toarray.js
@@ -1,28 +1,20 @@
-var test = require("tape");
-var toa = require("../toarray");
+import test from "tape";
+import { toarray as toa } from "../src/toarray";
 
-test("toarray", function (t) {
-    t.test("exports function", function (t) {
+test("toarray", t => {
+    t.test("exports function", t => {
         t.plan(1);
         t.equal(typeof toa, "function", "exports function");
     });
 
-    t.test("exports redundant api", function (t) {
-        t.plan(4);
-        t.ok(toa.toa === toa);
-        t.ok(toa.toA === toa);
-        t.ok(toa.toArray === toa);
-        t.ok(toa.toarray === toa);
-    });
-
-    t.test("arraylike", function (t) {
+    t.test("arraylike", t => {
         t.plan(3);
-        var arraylike = { length: 10 };
+        const arraylike = { length: 10 };
         t.ok(Array.isArray(toa(arraylike)));
         t.equal(10, toa(arraylike).length);
         t.equal(8, toa(arraylike, 2).length);
     });
-    t.test("arguments", function (t) {
+    t.test("arguments", t => {
         t.plan(3);
         !function () {
             t.ok(Array.isArray(toa(arguments)));
